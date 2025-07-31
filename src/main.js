ec885ff93bbbe205f6011688e3a502c97117ea9d
MaratFa <evolution99@rambler.ru>
2025-07-31 07:03:20 +0300

    edited

--- src/main.js
+++ src/main.js
@@ -2,30 +2,30 @@ const shop = document.getElementById("shop");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 const generateShop = () => {
-  return (shop.innerHTML = shopItemsData
+  shop.innerHTML = shopItemsData
     .map((item) => {
       const { id, name, price, desc, img } = item;
       const search = basket.find((x) => x.id === id);
 
       return `
-      <div class="item" id="product-id-${id}">
-      <img width="220" src="${img}" alt="" />
-      <div class="details">
-      <h3>${name}</h3>
-      <p>${desc}</p>
+        <div class="item" id="product-id-${id}">
+          <img width="220" src="${img}" alt="" />
+          <div class="details">
+            <h3>${name}</h3>
+            <p>${desc}</p>
             <div class="price-quantity">
-              <h2>$ ${price}
+              <h2>$ ${price}</h2>
               <div class="buttons">
-              <i class="bi bi-dash-lg" onclick="decrement('${id}')"></i>
-              <div class="quantity" id=${id}>${search ? search.item : 0}</div>
-              <i class="bi bi-plus-lg" onclick="increment('${id}')"></i>
+                <i class="bi bi-dash-lg" onclick="decrement('${id}')"></i>
+                <div class="quantity" id=${id}>${search ? search.item : 0}</div>
+                <i class="bi bi-plus-lg" onclick="increment('${id}')"></i>
               </div>
-              </div>
-              </div>
-              </div>
-              `;
+            </div>
+          </div>
+        </div>
+      `;
     })
-    .join(""));
+    .join("");
 };
 
 generateShop();
